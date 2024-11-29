35c85f462045a491d3983c98ef91b679ae88d1b8
so-uvik <souviksahamay27@gmail.com>
2024-11-29 22:05:27 +0530

    pushed auth middleware(test remaining)

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,1 +1,24 @@
-const authMiddleware = 
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('./config');
+
+
+const authMiddleware = (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || authHeader.split("")[0] != "Bearer") {
+    res.status(403).json({ message: "authorization Header format wrong or not present" })
+  }
+  const token = authHeader.split("")[1];
+
+  try {
+    const decoded = jwt.decode(token, JWT_SECRET);
+    req.userId = decoded;
+  }
+  catch (e) {
+    res.status(403).json({ message: "There has been an error verifying the token." });
+  }
+}
+
+module.exports = {
+  authMiddleware
+}
